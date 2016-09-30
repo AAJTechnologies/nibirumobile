678666e33ca781b9c4c55ec9eb64bffeb0d9598f
Lautaro Brasseur <lbrasseur@gmail.com>
2016-09-30 17:48:37 -0300

    Dagger 2 and GWT version upgrade

--- main/org.nibiru.mobile.gwt/src/main/java/org/nibiru/mobile/gwt/ioc/DefaultGwtModule.java
+++ main/org.nibiru.mobile.gwt/src/main/java/org/nibiru/mobile/gwt/ioc/DefaultGwtModule.java
@@ -6,12 +6,10 @@ import org.nibiru.mobile.core.api.event.EventBus;
 import org.nibiru.mobile.core.api.http.HttpManager;
 import org.nibiru.mobile.core.api.preferences.Preferences;
 import org.nibiru.mobile.core.api.ui.DisplayInfo;
-import org.nibiru.mobile.core.api.ui.Looper;
 import org.nibiru.mobile.gwt.event.GwtEventBus;
 import org.nibiru.mobile.gwt.http.RequestBuilderHttpManager;
 import org.nibiru.mobile.gwt.preferences.CookiesPreferences;
 import org.nibiru.mobile.gwt.ui.GwtDisplayInfo;
-import org.nibiru.mobile.gwt.ui.SchedulerLooper;
 
 import com.google.web.bindery.event.shared.SimpleEventBus;
 
@@ -40,11 +38,6 @@ public class DefaultGwtModule {
 	@Provides
 	public HttpManager getHttpManager(RequestBuilderHttpManager manager) {
 		return manager;
-	}
-
-	@Provides
-	public Looper getLooper(SchedulerLooper looper) {
-		return looper;
 	}
 
 	@Provides
