e82abe9410a1a6f73d22517de47a6a3600aa55da
Lautaro Brasseur <lbrasseur@gmail.com>
2018-04-09 16:27:32 -0300

    OkHttp timeout increase

--- org.nibiru.mobile.java/src/main/java/org/nibiru/mobile/java/ioc/DefaultJavaModule.java
+++ org.nibiru.mobile.java/src/main/java/org/nibiru/mobile/java/ioc/DefaultJavaModule.java
@@ -7,29 +7,35 @@ import org.nibiru.mobile.java.http.okhttp.OkHttpHttpManager;
 import org.nibiru.mobile.java.serializer.jackson.JacksonSerializer;
 import org.nibiru.mobile.java.service.WebSocketPushServiceFactory;
 
+import java.util.concurrent.TimeUnit;
+
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.OkHttpClient;
 
 @Module
 public class DefaultJavaModule {
-	@Provides
-	public OkHttpClient  getOkHttpClient () {
-		return new OkHttpClient();
-	}
+    @Provides
+    public OkHttpClient getOkHttpClient() {
+        return new OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .build();
+    }
 
-	@Provides
-	public HttpManager getHttpManager(OkHttpHttpManager manager) {
-		return manager;
-	}
+    @Provides
+    public HttpManager getHttpManager(OkHttpHttpManager manager) {
+        return manager;
+    }
 
-	@Provides
-	public Serializer getSerializer(JacksonSerializer serializer) {
-		return serializer;
-	}
+    @Provides
+    public Serializer getSerializer(JacksonSerializer serializer) {
+        return serializer;
+    }
 
-	@Provides
-	public PushServiceFactory getPushServiceFactory(WebSocketPushServiceFactory webSocketPushServiceFactory) {
-		return webSocketPushServiceFactory;
-	}
+    @Provides
+    public PushServiceFactory getPushServiceFactory(WebSocketPushServiceFactory webSocketPushServiceFactory) {
+        return webSocketPushServiceFactory;
+    }
 }
