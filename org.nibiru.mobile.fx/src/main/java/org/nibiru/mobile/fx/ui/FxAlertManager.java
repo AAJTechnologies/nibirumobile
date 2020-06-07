31a1d44f29dd78030832fda47f101be4d95525a7
Lautaro Brasseur <lbrasseur@gmail.com>
2020-06-07 16:17:02 -0300

    FX confirmation dialog

--- org.nibiru.mobile.fx/src/main/java/org/nibiru/mobile/fx/ui/FxAlertManager.java
+++ org.nibiru.mobile.fx/src/main/java/org/nibiru/mobile/fx/ui/FxAlertManager.java
@@ -2,6 +2,8 @@ package org.nibiru.mobile.fx.ui;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
 import org.nibiru.mobile.core.api.common.Consumer;
 import org.nibiru.mobile.core.api.ui.AlertManager;
 
@@ -44,25 +46,15 @@ public class FxAlertManager implements AlertManager {
         checkNotNull(message);
         checkNotNull(callback);
 
-//        Dialog<> dialog = new Dialog<>();
-//        dialog.setTitle(title);
-//        dialog.setContentText(message);
-//        dialog.getDialogPane()
-//                .getButtonTypes()
-//                .add(ButtonType.OK);
-//        dialog.getDialogPane()
-//                .getButtonTypes()
-//                .add(ButtonType.CANCEL);
-//
-//        final Button btOk = (Button) dialog.getDialogPane().
-//                lookupButton(ButtonType.OK);
-//        btOk.addEventFilter(ActionEvent.ACTION, event -> {
-//            Optional<String> result = dialog.getResult()
-//            if (result.isPresent()) {
-//                callback.accept(result.get());
-//            }
-//        });
-//        dialog.show();
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(message);
+        TextField field = new TextField();
+        alert.setGraphic(field);
+        alert.showAndWait();
+        if (ButtonType.OK.equals(alert.getResult())) {
+            callback.accept(field.getText());
+        }
     }
 
     @Override
@@ -72,6 +64,12 @@ public class FxAlertManager implements AlertManager {
         checkNotNull(title);
         checkNotNull(message);
         checkNotNull(callback);
+
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(message);
+        alert.showAndWait();
+        callback.accept(ButtonType.OK.equals(alert.getResult()));
     }
 
     private void showAlert(String message,
