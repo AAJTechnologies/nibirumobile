5393c3f836db4cf6598b315e6fffe7818a5d9dc0
Lautaro Braseur <lbrasseur@gmail.com>
2020-09-05 12:51:42 -0300

    Stage style adjustment

--- org.nibiru.mobile.fx/src/main/java/org/nibiru/mobile/fx/app/BaseFxApplication.java
+++ org.nibiru.mobile.fx/src/main/java/org/nibiru/mobile/fx/app/BaseFxApplication.java
@@ -2,13 +2,15 @@ package org.nibiru.mobile.fx.app;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.nibiru.mobile.core.api.app.Bootstrap;
 
 public abstract class BaseFxApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("CHANGEME");
         primaryStage.setMaximized(true);
+        primaryStage.setFullScreen(true);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
 
         getBootstrap(primaryStage).onBootstrap();
