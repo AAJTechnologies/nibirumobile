a1bf64229e04f4121b591c5468ee7dfb38bce5c9
Lautaro Brasseur <lbrasseur@gmail.com>
2020-09-27 08:59:17 -0300

    FX alert fix

--- org.nibiru.mobile.fx/src/main/java/org/nibiru/mobile/fx/ui/FxAlertManager.java
+++ org.nibiru.mobile.fx/src/main/java/org/nibiru/mobile/fx/ui/FxAlertManager.java
@@ -1,81 +1,132 @@
 package org.nibiru.mobile.fx.ui;
 
+import javafx.application.Platform;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.nibiru.mobile.core.api.common.Consumer;
 import org.nibiru.mobile.core.api.ui.AlertManager;
 
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 // TODO: Internationalization.
-public class FxAlertManager implements AlertManager {
+public class FxAlertManager
+        implements AlertManager {
+    private final Stage primaryStage;
+
     @Inject
-    public FxAlertManager() {
+    public FxAlertManager(@Nonnull Stage primaryStage) {
+        this.primaryStage = checkNotNull(primaryStage);
     }
 
     @Override
-    public void showMessage(String message) {
+    public void showMessage(@Nonnull String message) {
         checkNotNull(message);
-        showAlert(message,
-                AlertType.NONE);
+        buildAlert(message,
+                AlertType.NONE)
+                .show();
     }
 
     @Override
-    public void showNotification(String message) {
-        showMessage(message);
-        showAlert(message,
-                AlertType.INFORMATION);
+    public void showNotification(@Nonnull String message) {
+        checkNotNull(message);
+        buildAlert(message,
+                AlertType.INFORMATION)
+                .show();
     }
 
     @Override
-    public void showException(Exception exception) {
+    public void showException(@Nonnull Exception exception) {
         checkNotNull(exception);
-        showAlert(exception.getMessage(),
-                AlertType.ERROR);
+        buildAlert(exception.getMessage(),
+                AlertType.ERROR)
+                .show();
     }
 
     @Override
-    public void prompt(String title,
-                       String message,
-                       Consumer<String> callback) {
+    public void prompt(@Nonnull String title,
+                       @Nonnull String message,
+                       @Nonnull Consumer<String> callback) {
         checkNotNull(title);
         checkNotNull(message);
         checkNotNull(callback);
 
-        Alert alert = new Alert(AlertType.CONFIRMATION);
-        alert.setTitle(title);
-        alert.setHeaderText(message);
         TextField field = new TextField();
-        alert.setGraphic(field);
-        alert.showAndWait();
-        if (ButtonType.OK.equals(alert.getResult())) {
-            callback.accept(field.getText());
-        }
+        buildAlert(title,
+                AlertType.CONFIRMATION)
+                .headerTex(message)
+                .graphic(field)
+                .show(alert -> {
+                    if (ButtonType.OK.equals(alert.getResult())) {
+                        callback.accept(field.getText());
+                    }
+                });
+
     }
 
     @Override
-    public void confirm(String title,
-                        String message,
-                        Consumer<Boolean> callback) {
+    public void confirm(@Nonnull String title,
+                        @Nonnull String message,
+                        @Nonnull Consumer<Boolean> callback) {
         checkNotNull(title);
         checkNotNull(message);
         checkNotNull(callback);
 
-        Alert alert = new Alert(AlertType.CONFIRMATION);
-        alert.setTitle(title);
-        alert.setHeaderText(message);
-        alert.showAndWait();
-        callback.accept(ButtonType.OK.equals(alert.getResult()));
+        buildAlert(title,
+                AlertType.CONFIRMATION)
+                .headerTex(message)
+                .show(alert -> callback.accept(ButtonType.OK
+                        .equals(alert.getResult())));
     }
 
-    private void showAlert(String message,
-                           AlertType type) {
-        Alert alert = new Alert(type);
-        alert.setTitle(message);
-        alert.showAndWait();
+    private AlertBuilder buildAlert(String message,
+                                    AlertType type) {
+        return new AlertBuilder(primaryStage,
+                message,
+                type);
+    }
+
+    private static class AlertBuilder {
+        private final Alert alert;
+
+        private AlertBuilder(Stage primaryStage,
+                             String message,
+                             AlertType type) {
+            alert = new Alert(type);
+            alert.initOwner(primaryStage.getScene()
+                    .getWindow());
+            alert.setTitle(message);
+        }
+
+        private AlertBuilder headerTex(String message) {
+            alert.setHeaderText(message);
+            return this;
+        }
+
+        private AlertBuilder graphic(Node graphic) {
+            alert.setGraphic(graphic);
+            return this;
+        }
+
+        private AlertBuilder show(Consumer<Alert> callback) {
+            Platform.runLater(() -> {
+                alert.showAndWait();
+                callback.accept(alert);
+            });
+            return this;
+        }
+
+        private AlertBuilder show() {
+            Platform.runLater(() -> {
+                alert.show();
+            });
+            return this;
+        }
     }
 }
